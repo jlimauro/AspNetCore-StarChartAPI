8ea558d070827cfc7c6978cdce266010cfedc808
Jeffrey Limauro <jlimauro@gmail.com>
2019-12-31 18:19:57 -0500

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,63 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var obj =  _context.CelestialObjects.Where(x => x.Id == id).FirstOrDefault();
+
+            if (obj != null)
+            {
+                obj.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var obj = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+
+            if (obj.Any())
+            {
+                foreach(var o in obj)
+                {
+                    o.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == o.Id).ToList();
+                }
+
+                return Ok(obj);
+
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var obj = _context.CelestialObjects.ToList();
+
+            if (obj.Any())
+            {
+                foreach (var o in obj)
+                {
+                    o.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == o.Id).ToList();
+                }
+
+                return Ok(obj);
+
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     } 
 }
